d5f7aea206b4944cc2d3095b7aff7d1473d3a83d
soaringleefighting <leeyuanpeng@hotmail.com>
2021-07-10 17:59:39 +0800

    refactor:1.修复windows平台64位编译问题。

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 /****************************************************************** 
-    Kunpeng Technology CO. LTD 
-    2010-2020 Copyright reversed 
-    File			:	revison.h 
-    Description	:	this file is revison information.  
-    Author		:	lipeng 
-    Modified		:	2020.8.16	Created 
-********************************************************************/ 
+/*    Kunpeng Technology CO. LTD 
+/*    2010-2020 Copyright reversed 
+/*    File			:	revison.h 
+/*    Description	:	this file is revison information.  
+/*    Author		:	lipeng 
+/*    Modified		:	2020.8.16	Created 
+/********************************************************************/ 
 #ifndef VERSION_H_  
 #define VERSION_H_  
 //major.minor.svn(git)version.datetime_base   
@@ -13,8 +13,8 @@
 #define MINOR_VERSION	("1")  
 #if _WIN32  
 #define SG_BRANCH_NAME	("*master")  
-#define SG_COMMIT_HASH	("b95c546")  
-#define SG_COMMIT_DATE	("2021-07-0812:48:42+0000")  
+#define SG_COMMIT_HASH	("d31b43d")  
+#define SG_COMMIT_DATE	("2021-07-0923:14:59+0800")  
 #define SG_VERSION		(SG_COMMIT_HASH)  
 #else  
 #ifdef ANDROID 
